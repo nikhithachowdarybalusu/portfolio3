cea8e78f0d633dffe20a6db7082f8549ce5a25f8
nikhithachowdarybalusu <nikhithachowdarybalusu@gmail.com>
2025-03-05 17:43:59 -0600

    Edited app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio3';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  scrollToSection(sectionId: string) {
+    this.toggleMenu();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      window.history.pushState({}, '', `#${sectionId}`);  // Update the URL
+    }
+  }
 }
