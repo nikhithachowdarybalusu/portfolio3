442d44eebf5afa1e77c2762e0468d6f1c63fd5f4
nikhithachowdarybalusu <nikhithachowdarybalusu@gmail.com>
2025-03-13 14:52:35 -0500

    Accordion styles adjusted

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -20,17 +20,33 @@ export class EducationComponent implements OnInit {
       title: 'Bachelor of Science in Computer Science',
       institution: 'XYZ University',
       description: 'Focus on algorithms, data structures, and programming languages.',
+      duration: '2017-2021',
       delay: '0.2s'
     },
     {
       title: 'Master of Science in Computer Science',
       institution: 'ABC Institute of Technology',
       description: 'Advanced topics in machine learning, AI, and data analysis.',
+      duration: '2017-2021',
       delay: '0.5s'
     }
   ];
+  activeIndex: number | null = null;
+
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  // Toggle accordion open and close
+  toggleAccordion(index: number): void {
+    // If the clicked accordion is already open, close it (set activeIndex to null)
+    // Else, set the activeIndex to the clicked accordion's index
+    if (this.activeIndex === index) {
+      this.activeIndex = null; // Close the currently open accordion
+    } else {
+      this.activeIndex = index; // Open the clicked accordion
+    }
+  }
+  
 }
